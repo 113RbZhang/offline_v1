6c0afd68ba708080512f77bd52c0d8aff82c768a
zrb0604 <1130109940@qq.com>
2025-03-24 21:22:51 +0800

    采集数据

---
+++ shells/mysql_to_hdfs.sh
@@ -0,0 +1,21 @@
+for i in activity_info base_category3\
+                                base_dic\
+                                cart_info\
+                                order_detail\
+                                order_info\
+                                sku_info\
+                                spu_info\
+                                user_info\
+
+do
+sqoop import --connect jdbc:mysql://192.168.58.1/gmall \
+      --username root --password root \
+      --table $i \
+      --target-dir /2207A/runbo_zhang/gmall/yewu/$i \
+      -m 1 -z --compression-codec lzop \
+      --delete-target-dir \
+      --null-string '\\N' --null-non-string '\\N' \
+      --fields-terminated-by '\t' --lines-terminated-by '\n'
+
+
+done
