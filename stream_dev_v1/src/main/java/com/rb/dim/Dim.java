9c93c5fd9a6264e25035242fff1fccaa00bbb665
zrb0604 <1130109940@qq.com>
2025-04-09 22:54:21 +0800

    r

--- stream_dev_v1/src/main/java/com/rb/dim/Dim.java
+++ stream_dev_v1/src/main/java/com/rb/dim/Dim.java
@@ -3,6 +3,7 @@ package com.rb.dim;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.rb.CdcSource;
+import com.rb.utils.HbaseUtil;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.common.state.BroadcastState;
 import org.apache.flink.api.common.state.MapStateDescriptor;
@@ -11,15 +12,19 @@ import org.apache.flink.api.common.typeinfo.BasicTypeInfo;
 import org.apache.flink.api.common.typeinfo.TypeHint;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.connector.base.DeliveryGuarantee;
 import org.apache.flink.connector.kafka.sink.KafkaRecordSerializationSchema;
 import org.apache.flink.connector.kafka.sink.KafkaSink;
 import org.apache.flink.streaming.api.datastream.*;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.co.BroadcastProcessFunction;
+import org.apache.flink.streaming.api.functions.co.KeyedBroadcastProcessFunction;
+import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
 import org.apache.flink.streaming.api.functions.sink.SinkFunction;
 import org.apache.flink.util.Collector;
 import org.apache.flink.util.OutputTag;
+import org.apache.hadoop.hbase.client.Connection;
 
 import static com.rb.CdcSource.kafkaRead;
 
@@ -42,61 +47,119 @@ public class Dim {
         MapStateDescriptor<String, String> dimRuleStateDescriptor = new MapStateDescriptor<>(
                 "online_flink_retail_process",
                 BasicTypeInfo.STRING_TYPE_INFO,
-                TypeInformation.of(new TypeHint<String>() {}));
+                TypeInformation.of(new TypeHint<String>() {
+                }));
         //声明为广播流
         BroadcastStream<String> broadcastStream = processData.broadcast(dimRuleStateDescriptor);
         //cdc流连接广播流
-        BroadcastConnectedStream<String, String> connectedStream = dbData.connect(broadcastStream);
+        KeyedStream<String, String> allData = dbData.keyBy(v -> "aa");
+        BroadcastConnectedStream<String, String> connectedStream = allData.connect(broadcastStream);
 
         OutputTag<String> dwdOutputTag = new OutputTag<String>("dwd_table") {
         };
         //通过广播流过滤维度表
-        SingleOutputStreamOperator<Tuple2<String, String>> dimTablesStream = connectedStream.process(new BroadcastProcessFunction<String, String, Tuple2<String, String>>() {
+//        SingleOutputStreamOperator<Tuple2<String, String>> dimTablesStream =
+        SingleOutputStreamOperator<Tuple2<String, String>> dimTablesStream = connectedStream
+                .process(new KeyedBroadcastProcessFunction<String, String, String, Tuple2<String, String>>() {
+                    //KeyedBroadcastProcessFunction
+                    @Override
+                    public void processElement(String value, KeyedBroadcastProcessFunction<String, String, String, Tuple2<String, String>>.ReadOnlyContext ctx, Collector<Tuple2<String, String>> out) throws Exception {
+                        JSONObject object = JSON.parseObject(value);
+                        String table = object.getJSONObject("source").getString("table");
+                        ReadOnlyBroadcastState<String, String> broadcastState = ctx.getBroadcastState(dimRuleStateDescriptor);
+
+                        String process = broadcastState.get(table);
+                        String after = object.getString("after");
+                        //广播变量中没有数据则延迟五秒处理
+
+                        if (!broadcastState.immutableEntries().iterator().hasNext()) {
+                            long l = ctx.timerService().currentProcessingTime() + 5000;
+                            ctx.timerService().registerProcessingTimeTimer(l);
+                        } else {
+                            if (process != null) {//获取到配置信息则是维度表
+                                out.collect(Tuple2.of(after, process));
+                            } else {
+                                ctx.output(dwdOutputTag, value);
+                            }
+                            ctx.timerService().deleteProcessingTimeTimer(ctx.timerService().currentProcessingTime());
+                        }
+
+
+                    }
+
+                    @Override
+                    public void onTimer(long timestamp, KeyedBroadcastProcessFunction<String, String, String, Tuple2<String, String>>.OnTimerContext ctx, Collector<Tuple2<String, String>> out) throws Exception {
+
+                        ReadOnlyBroadcastState<String, String> broadcastState = ctx.getBroadcastState(dimRuleStateDescriptor);
+                        if (!broadcastState.immutableEntries().iterator().hasNext()) {
+                            //如果还是空则继续延迟5s
+                            long l = ctx.timerService().currentProcessingTime() + 5000;
+                            ctx.timerService().registerProcessingTimeTimer(l);
+                        }
+                    }
+
+                    @Override
+                    public void processBroadcastElement(String value, KeyedBroadcastProcessFunction<String, String, String, Tuple2<String, String>>.Context ctx, Collector<Tuple2<String, String>> out) throws Exception {
+                        JSONObject dim_process = JSON.parseObject(value);
+                        String op = dim_process.getString("op");
+
+                        String table = dim_process.getJSONObject("after").getString("source_table");
+                        BroadcastState<String, String> broadcastState = ctx.getBroadcastState(dimRuleStateDescriptor);
+
+                        if ("d".equals(op)) {//如果是删除则删除这个表
+                            broadcastState.remove(table);
+                        } else {
+                            //如果不是则添加
+
+                            broadcastState.put(table, value);
+                        }
+
+                    }
+                });
+        SideOutputDataStream<String> sideOutput = dimTablesStream.getSideOutput(dwdOutputTag);
+
+        //zhuliu> ({"birthday":4937,"create_time":1744240721000,"login_name":"3hd4p2a","nick_name":"春春","name":"尹春","user_level":"1","phone_num":"13447522216","id":150,"email":"3hd4p2a@0355.net"}
+        // {"before":null,"after":{"source_table":"user_info","sink_table":"dim_user_info","sink_family":"info","sink_columns":"id,login_name,name,user_level,birthday,gender,create_time,operate_time","sink_row_key":"id"},"source":{"version":"1.6.4.Final","connector":"mysql","name":"mysql_binlog_source","ts_ms":0,"snapshot":"false","db":"online_flink_retail_process","sequence":null,"table":"table_process_dim","server_id":0,"gtid":null,"file":"","pos":0,"row":0,"thread":null,"query":null},"op":"r","ts_ms":1744196070945,"transaction":null})
+//        dimTablesStream.print("zhuliu");
+        //celiu> {"before":null,"after":{"id":5251,"log":"{\"common\":{\"ar\":\"28\",\"ba\":\"OPPO\",\"ch\":\"web\",\"is_new\":\"0\",\"md\":\"OPPO Remo8\",\"mid\":\"mid_302\",\"os\":\"Android 13.0\",\"sid\":\"76ce22b1-057e-49e3-a55d-fcd792d91721\",\"uid\":\"66\",\"vc\":\"v2.1.134\"},\"page\":{\"during_time\":17628,\"last_page_id\":\"mine\",\"page_id\":\"order_list\"},\"ts\":1744210322533}"},"source":{"version":"1.6.4.Final","connector":"mysql","name":"mysql_binlog_source","ts_ms":0,"snapshot":"false","db":"online_flink_retail","sequence":null,"table":"z_log","server_id":0,"gtid":null,"file":"","pos":0,"row":0,"thread":null,"query":null},"op":"r","ts_ms":1744188374568,"transaction":null}
+//        sideOutput.print("celiu");
+
+
+
+
+        dimTablesStream.addSink(new RichSinkFunction<Tuple2<String, String>>() {
+            private Connection hbaseConnection;
             @Override
-            public void processElement(String value, BroadcastProcessFunction<String, String, Tuple2<String, String>>.ReadOnlyContext ctx, Collector<Tuple2<String, String>> out) throws Exception {
-                JSONObject object = JSON.parseObject(value);
-                String table = object.getJSONObject("source").getString("table");
-                ReadOnlyBroadcastState<String, String> broadcastState = ctx.getBroadcastState(dimRuleStateDescriptor);
-                String process = broadcastState.get(table);
-                String after = object.getString("after");
-                if (process != null) {//获取到配置信息则是维度表
-                    out.collect(Tuple2.of(after, process));
-                }else {
-                    ctx.output(dwdOutputTag, value);
-                }
+            public void open(Configuration parameters) throws Exception {
+                hbaseConnection = HbaseUtil.getHbaseConnection();
             }
 
             @Override
-            public void processBroadcastElement(String value, BroadcastProcessFunction<String, String, Tuple2<String, String>>.Context ctx, Collector<Tuple2<String, String>> out) throws Exception {
-                JSONObject dim_process = JSON.parseObject(value);
-                String op = dim_process.getString("op");
+            public void close() throws Exception {
+                HbaseUtil.closeHbaseConnection(hbaseConnection);
+            }
 
-                String table = dim_process.getJSONObject("after").getString("source_table");
-                BroadcastState<String, String> broadcastState = ctx.getBroadcastState(dimRuleStateDescriptor);
-                if ("d".equals(op)) {//如果是删除则删除这个表
-                    broadcastState.remove(table);
-                } else {
-                    //如果不是则添加
-                    broadcastState.put(table, value);
+            @Override
+            public void invoke(Tuple2<String, String> tp) throws Exception {
+                String data = tp.f0;
+                JSONObject dataJson = JSON.parseObject(data);
+                String process = tp.f1;
+                JSONObject processJson = JSON.parseObject(process);
+                String op = processJson.getString("op");
+                String sinkTable = processJson.getJSONObject("after").getString("sink_table");
+                String sinkRowKey = dataJson.getString(processJson.getJSONObject("after").getString("sink_row_key"));
+                if ("d".equals(op)){//删除操作
+
+                    HbaseUtil.delRow(hbaseConnection,"dim_zrb_online_v1",sinkTable,sinkRowKey);
+
+                }else {//不是删除操作
+                    String family = processJson.getJSONObject("after").getString("sink_family");
+                    HbaseUtil.putRow(hbaseConnection, "dim_zrb_online_v1", sinkTable, sinkRowKey,family,dataJson.toJSONString() );
                 }
+
 
             }
         });
-        SideOutputDataStream<String> sideOutput = dimTablesStream.getSideOutput(dwdOutputTag);
-
-        dimTablesStream.print("zhuliu");
-
-//        data.broadcast()
-
-//        dimTablesStream.addSink(new SinkFunction<Tuple2<String, String>>() {
-//            @Override
-//            public void invoke(Tuple2<String, String> tp) throws Exception {
-//                String data = tp.f0;
-//                String process = tp.f1;
-//
-//
-//            }
-//        })
 
 
         env.disableOperatorChaining();
---
+++ stream_dev_v1/src/main/java/com/rb/utils/HbaseUtil.java
@@ -0,0 +1,134 @@
+package com.rb.utils;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.util.Bytes;
+
+import java.io.IOException;
+import java.util.Set;
+
+/**
+ * @Package com.rb.utils.HbaseUtil
+ * @Author runbo.zhang
+ * @Date 2025/4/9 8:46
+ * @description:
+ */
+public class HbaseUtil {
+    //todo 连接hbase  获取连接
+    public static Connection getHbaseConnection() throws IOException {
+
+        Configuration conf = new Configuration();
+        conf.set("hbase.zookeeper.quorum", "cdh01,cdh02,cdh03");
+        conf.set("hbase.zookeeper.property.clientPort", "2181");
+
+        Connection connection = ConnectionFactory.createConnection(conf);
+        return connection;
+    }
+
+    //todo 关闭连接
+    public static void closeHbaseConnection(Connection connection) throws IOException {
+
+        if (connection != null && !connection.isClosed()) {
+            connection.close();
+        }
+    }
+
+    //todo 建表
+    public static void createTable(Connection connection, String namespace, String tableName, String... families) {
+        if (families.length < 1) {
+            System.out.println("至少要有一个列族");
+            return;
+        }
+        try (Admin admin = connection.getAdmin()) {
+            TableName tableNameObj = TableName.valueOf(namespace, tableName);
+            if (admin.tableExists(tableNameObj)) {
+                System.out.println(namespace + "." + tableName + "表已经存在");
+                return;
+            }
+            TableDescriptorBuilder tableDescriptorBuilder = TableDescriptorBuilder.newBuilder(tableNameObj);
+
+            for (String family : families) {//添加列族
+                ColumnFamilyDescriptor build = ColumnFamilyDescriptorBuilder.newBuilder(Bytes.toBytes(family)).build();
+                tableDescriptorBuilder.setColumnFamily(build);
+            }
+            //建表
+            admin.createTable(tableDescriptorBuilder.build());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //todo 删除表
+    public static void dropTable(Connection connection, String namespace, String tableName) {
+        try (Admin admin = connection.getAdmin()) {
+            TableName tableNameObj = TableName.valueOf(namespace, tableName);
+            if (!admin.tableExists(tableNameObj)) {
+                System.out.println("要删除的" + namespace + "." + tableName + "表不存在");
+                return;
+            }
+            admin.disableTable(tableNameObj);
+            admin.deleteTable(tableNameObj);
+            System.out.println("要删除的" + namespace + "." + tableName + "表删除成功");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    //todo put数据
+
+    /**
+     * @param connection 连接
+     * @param namespace  库
+     * @param tableName  表名
+     * @param rowKey     列编号
+     * @param family     列族
+     * @param jsonData   数据
+     */
+    public static void putRow(Connection connection, String namespace, String tableName, String rowKey, String family, String jsonData) {
+        //合成库名.表名
+        TableName tableNameObj = TableName.valueOf(namespace, tableName);
+        try {
+            //连接表
+            Table table = connection.getTable(tableNameObj);
+            Admin admin = connection.getAdmin();
+            if (!admin.tableExists(tableNameObj)){
+                createTable(connection, namespace, tableName, family);
+            }
+            //生成put对象
+            Put put = new Put(Bytes.toBytes(rowKey));
+            JSONObject data = JSON.parseObject(jsonData);
+            Set<String> keys = data.keySet();
+            for (String key : keys) {
+                String value = data.getString(key);
+                if (StringUtils.isNotEmpty(value)) {
+                    //value不为空就把数据添加进put
+                    put.addColumn(Bytes.toBytes(family), Bytes.toBytes(key), Bytes.toBytes(value));
+                }
+            }
+            table.put(put);
+            System.out.println("数据添加到" + namespace + "." + tableName + "表成功");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //todo 删除数据
+    public static void delRow(Connection connection, String namespace, String tableName, String rowKey) {
+        //合成库名.表名
+        TableName tableNameObj = TableName.valueOf(namespace, tableName);
+        try(Table table = connection.getTable(tableNameObj)) {
+
+            Delete delete = new Delete(Bytes.toBytes(rowKey));
+
+            table.delete(delete);
+
+
+            System.out.println("删除" + namespace + "." + tableName + "表的数据成功");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
